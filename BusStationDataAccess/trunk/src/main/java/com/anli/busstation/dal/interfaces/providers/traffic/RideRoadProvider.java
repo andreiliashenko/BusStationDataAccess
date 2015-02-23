1f3d0bfa50c14f861189eabbd8b520e4466e7639
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-02-23 22:49:05 +0000

    RideRoadProvider interface finder method signatures fixed

--- BusStationDataAccess/trunk/src/main/java/com/anli/busstation/dal/interfaces/providers/traffic/RideRoadProvider.java
+++ BusStationDataAccess/trunk/src/main/java/com/anli/busstation/dal/interfaces/providers/traffic/RideRoadProvider.java
@@ -5,23 +5,24 @@ import com.anli.busstation.dal.interfaces.entities.traffic.RideRoad;
 import com.anli.busstation.dal.interfaces.entities.geography.Road;
 import com.anli.busstation.dal.interfaces.providers.BSEntityProvider;
 import java.math.BigInteger;
+import java.util.Collection;
 import java.util.List;
 
 public interface RideRoadProvider extends BSEntityProvider<RideRoad> {
 
     List<RideRoad> findByRoad(Road road);
 
-    List<RideRoad> findByAnyRoad(List<Road> roadList);
+    List<RideRoad> findByAnyRoad(Collection<Road> roadList);
 
     List<RideRoad> findByDriver(Driver driver);
 
-    List<RideRoad> findByAnyDriver(List<Driver> driverList);
+    List<RideRoad> findByAnyDriver(Collection<Driver> driverList);
 
     List<BigInteger> collectIdsByRoad(Road road);
 
-    List<BigInteger> collectIdsByAnyRoad(List<Road> roadList);
+    List<BigInteger> collectIdsByAnyRoad(Collection<Road> roadList);
 
     List<BigInteger> collectIdsByDriver(Driver driver);
 
-    List<BigInteger> collectIdsByAnyDriver(List<Driver> driverList);
+    List<BigInteger> collectIdsByAnyDriver(Collection<Driver> driverList);
 }
